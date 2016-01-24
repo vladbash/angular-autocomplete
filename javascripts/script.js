dda3cc28d076fbd7f26297a7a92746da2e25f17d
vladb95_ <vladb95z-soft@yandex.ru>
2016-01-24 17:55:34 +0200

    added demo directive

---
+++ javascripts/script.js
@@ -0,0 +1,10 @@
+angular.module('demoApp',['auto-complete']);
+
+angular.module('demoApp').controller('demoCtrl',demoController);
+
+demoController.$inject=['$scope'];
+function demoController($scope){
+	$scope.input='';
+	$scope.data=["john", "bill", "charlie", "robert", "alban", "oscar", "marie", "celine", "brad", "drew", "rebecca", "michel", "francis", "jean", "paul", "pierre", "nicolas", "alfred"];
+  $scope.color='#159957';
+}
